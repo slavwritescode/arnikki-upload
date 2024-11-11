acee71767528fb6e39eedd8f6dd4589d48896c1b
slavwritescode <preslav.getov@telusinternational.com>
2024-11-11 13:43:10 +0200

    register

--- src/components/RegistrationPage/index.jsx
+++ src/components/RegistrationPage/index.jsx
@@ -1,14 +1,39 @@
+import { useForm } from "react-hook-form"
+
 import './index.css';
 
 const RegistrationPage = () => {
-    return <div id="container">
-        
-        <p>Please enter your information to register for our program</p>
-        <form>
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm();
 
-        </form>
-        
-    </div>
+    const handleRegistration = (data) => {
+        console.log(data);
+    }
+    console.log(watch("example"));
+
+    return <div id="registrationPage">
+                <div id="registrationContainer">
+                    <p>Please enter your information to register for our program</p>
+                    <form onSubmit={handleSubmit(handleRegistration)}>
+                        <input 
+                            type="text"
+                            placeholder="email"
+                            {...register("email", { required: true })} 
+                        />
+                        <input 
+                            type="password"
+                            placeholder="password"
+                            {...register("password", { required: true })} 
+                        />
+                        {errors.email && <span>The email field is mandatory</span>}
+                        <button type="submit">Register</button>
+                    </form>
+                </div>
+            </div>
 }
 
 export default RegistrationPage;
