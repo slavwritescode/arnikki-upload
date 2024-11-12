0a9dad1bb411e0958c1e0102353a5e901f6da3a6
slavwritescode <preslav.getov@telusinternational.com>
2024-11-12 15:51:19 +0200

    video

--- src/components/VideoTagging/index.jsx
+++ src/components/VideoTagging/index.jsx
@@ -1,13 +1,25 @@
 import { useForm } from "react-hook-form";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import './index.css';
 
 const VideoTagging = () => {
+  const [videoFile, setVideoFile] = useState(null);
+  const [videoUrl, setVideoUrl] = useState(null);
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm();  
+  
+  const handleFileChange = (event) => {
+    setVideoFile(event.target.files[0]);  // Set the selected file in state
+  };
+
+  const removeFile = () => {
+    setVideoFile(null);
+  }
 
   useEffect(()=>{
 
@@ -16,19 +28,26 @@ const VideoTagging = () => {
     <div id="videoTaggingPage">
       {/**have a list for all previous uploaded videos */}
       <div id="allVideosByUserContainer">
+        <p>All recently uploaded videos</p>
 
       </div>
       <div id="uploadVideoContainer">
-        <p>All recently uploaded videos</p>
         {/**have a form for uploading a video */}
-        <form>
+        {videoFile && (
+          <div>
+            <p>Selected file: {videoFile.name}</p>
+            <button type="button" onClick={removeFile}>Remove File</button>
+          </div>
+        )}
+        <form onChange={handleFileChange}>
         <label htmlFor="video_file">Choose a file to upload</label>
         <input 
           type="file" 
           id="video_file" 
           name="video_file" 
-          accept="image/png, image/jpeg" />
+          accept="video/mov" />
         </form>
+        
       </div>
         
 
