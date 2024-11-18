96132790f8def2393df49162c7abcc5fba6231d0
slavwritescode <preslav.getov@telusinternational.com>
2024-11-18 11:23:29 +0200

    style

--- src/components/VideoTagging/VideoPreview/index.jsx
+++ src/components/VideoTagging/VideoPreview/index.jsx
@@ -30,56 +30,55 @@ const VideoPreview = ({ videoUrl }) => {
 
     return (<div id="videoPreview">
         {isClicked ? <>
-            <button id="backButton" onClick={() => setIsClicked(value => !value)}>Go back</button>
-            <video controls width="500" src={url} />
 
-            <form onSubmit={handleSubmit(review)} autoComplete="off">
-                <select
-                    {...register("scenarios", {
-                        required: true,
-                        message: "Select a relevant scenario"
-                    })}
-                >
-                    {Object.values(Constants['scenarios']).map((scenarioItem) => <option key={scenarioItem}>{scenarioItem}</option>)}
-                </select>
-                <select
-                    {...register("deviceHeight", {
-                        required: true,
-                        message: "Select a height"
-                    })}
-                >
-                    {Object.values(Constants['deviceHeight']).map((deviceHeightItem) => <option key={deviceHeightItem}>{deviceHeightItem}</option>)}
-                </select>
-                <select
-                    {...register("lighting", {
-                        required: true,
-                        message: "Select a type of lighting"
-                    })}
-                >
-                    {Object.values(Constants['lighting']).map((lightingItem) => <option key={lightingItem}>{lightingItem}</option>)}
-                </select>
-                <select
-                    {...register("angle", {
-                        required: true,
-                        message: "Select an approach angle"
-                    })}
-                >
-                    {Object.values(Constants['approachAngle']).map((approachAngleItem) => <option key={approachAngleItem}>{approachAngleItem}</option>)}
-                </select>
-                <div className="labelContainer">
-                    <label htmlFor='clothingSelect'>Clothing type</label>
+            <video controls width="500" src={url} />
+            <div className="controls">
+                <button id="backButton" onClick={() => setIsClicked(value => !value)}>Go back</button>
+                <form onSubmit={handleSubmit(review)} autoComplete="off">
                     <select
-                        id="clothingSelect"
-                        {...register("clothing", {
+                        {...register("scenarios", {
                             required: true,
-                            message: "Select a clothing type"
+                            message: "Select a relevant scenario"
                         })}
                     >
-                        {Object.values(Constants['clothing']).map((clothingItem) => <option key={clothingItem}>{clothingItem}</option>)}
+                        {Object.values(Constants['scenarios']).map((scenarioItem) => <option key={scenarioItem}>{scenarioItem}</option>)}
                     </select>
-                </div>
-                <button type="submit" id="reviewButton">Confirm selections</button>
-            </form></> : <button id="reviewButton" onClick={() => setIsClicked(value => !value)}>Review this video item</button>}
+                    <select
+                        {...register("deviceHeight", {
+                            required: true,
+                            message: "Select a height"
+                        })}
+                    >
+                        {Object.values(Constants['deviceHeight']).map((deviceHeightItem) => <option key={deviceHeightItem}>{deviceHeightItem}</option>)}
+                    </select>
+                    <select
+                        {...register("lighting", {
+                            required: true,
+                            message: "Select a type of lighting"
+                        })}
+                    >
+                        {Object.values(Constants['lighting']).map((lightingItem) => <option key={lightingItem}>{lightingItem}</option>)}
+                    </select>
+                    <select
+                        {...register("angle", {
+                            required: true,
+                            message: "Select an approach angle"
+                        })}
+                    >
+                        {Object.values(Constants['approachAngle']).map((approachAngleItem) => <option key={approachAngleItem}>{approachAngleItem}</option>)}
+                    </select>
+
+                    <fieldset>
+                        <legend>Choose all clothing that applies</legend>
+                        {Object.values(Constants['clothing']).map(clothingItem => <div key={clothingItem}>
+                            <input type="checkbox" id={clothingItem} value={clothingItem} />
+                            <label htmlFor={clothingItem}>{clothingItem}</label>
+                        </div>)}
+                    </fieldset>
+                    <button type="submit" id="reviewButton">Confirm selections</button>
+                </form>
+            </div>
+        </> : <button id="reviewButton" onClick={() => setIsClicked(value => !value)}>Review this video item</button>}
     </div>)
 }
 
